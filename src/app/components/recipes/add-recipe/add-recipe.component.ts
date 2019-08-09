739b123f0807bdb3a469f72044727ec90bb4a5de
Joel Nathanael Barriga Polzin <joel.bapo@gmail.com>
2019-08-09 02:17:14 +0200

    recipe form and ingredient selection

---
+++ src/app/components/recipes/add-recipe/add-recipe.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-add-recipe',
+  templateUrl: './add-recipe.component.html',
+  styleUrls: ['./add-recipe.component.css']
+})
+export class AddRecipeComponent implements OnInit {
+
+  recipe: Recipe = {
+    title: '',
+    picture: '',
+    instruction: ''
+  }
+  ingredient = [];
+
+  isLinear = false;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
+  forthFormGroup: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+    this.thirdFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+    this.forthFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+  }
+
+}
